b7a37705fa5a920dc95d80729fe325370c5298b6
Josiah Augustine <josiah.augustine.o@gmail.com>
2023-05-12 13:07:37 +0100

    Update: schema module
    add validator to ensure all fields are digits.

--- backend/schema.py
+++ backend/schema.py
@@ -1,5 +1,5 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class CreditCardSchema(BaseModel):
@@ -7,3 +7,14 @@ class CreditCardSchema(BaseModel):
     expiry_year: int
     cvv: str
     card_number: str
+
+    @validator("expiry_month", "expiry_year", "cvv", "card_number")
+    def validate_all_fields_are_digits(cls, value):
+        # try casting value to integer then fail on error
+        # else return initial value
+        try:
+            int(value)
+        except:
+            raise ValueError("Value must be digits")
+        
+        return value
