909d41ec5c1e51a688a44294d0cbc8262566c626
Josiah Augustine <josiah.augustine.o@gmail.com>
2023-05-12 13:25:47 +0100

    Update: schema module
    only validate fields with string datatype with validator method.

--- backend/schema.py
+++ backend/schema.py
@@ -8,11 +8,12 @@ class CreditCardSchema(BaseModel):
     cvv: str
     card_number: str
 
-    @validator("expiry_month", "expiry_year", "cvv", "card_number")
+    @validator("cvv", "card_number")
     def validate_all_fields_are_digits(cls, value):
         # try casting value to integer then fail on error
         # else return initial value
         try:
+            print(value, int(value.lstrip("0")))
             int(value.lstrip("0"))
         except:
             raise ValueError("Value must be digits")
